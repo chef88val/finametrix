5fb0c93d16b2baf624471667b34b31a0ead29034
javiersegarra <javiersegarra@quibim.com>
2021-09-23 12:15:14 +0200

    - i18n

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { TableComponent } from './table/table.component';
+import { InternationalizationModule } from '../internationalization/internationalization.module';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { TableComponent } from './table/table.component';
   ],
   imports: [
     CommonModule,
+    InternationalizationModule,
     SharedRoutingModule
   ],
   exports: [TableComponent]
